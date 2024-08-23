03bb256b93bb35daecb7a87f3b867210b679aed1
sreeragh <sreeragh.s@icloud.com>
2024-08-23 22:40:58 +0530

    commit

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-interface ReferencePoint {
-    width: number;
-    scale: number;
-}
-export declare function calculateScale(viewportWidth: number, referencePoints: ReferencePoint[]): number;
-export declare function useViewportScale(referencePoints: ReferencePoint[]): number;
-export {};
